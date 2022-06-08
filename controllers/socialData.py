39dcce536e78762af089d5908a2c437ada627227
Jacob Molyneux <jacobmolyneux@Jacobs-MacBook-Pro.local>
2022-06-07 21:33:19 -0400

    added sentiment analysis to tweets when retreived

--- controllers/socialData.py
+++ controllers/socialData.py
@@ -1,6 +1,11 @@
 import requests
 import tweepy
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+BEARER_TOKEN = 'AAAAAAAAAAAAAAAAAAAAAGiKMwEAAAAA9IOJaFNMvj3x6FaqJQnoJyZ81Rs%3D8MVGwbqXMkCedrCMgSSlCguvPXqKBKQovKm8ZjaEqMe1N1sbs2'
 
+
+
+    
 
 
 auth = tweepy.OAuth2BearerHandler(BEARER_TOKEN)
@@ -9,11 +14,13 @@ api = tweepy.API(auth)
 def get_tweets(term:str):
     response = api.search_tweets(term)
     tweet_data = []
+    analyzer = SentimentIntensityAnalyzer()
     for tweet in response:
         tweet_data.append({
-            "Text":tweet._json['text'],
             "Date":tweet._json['created_at'],
-            "User":tweet._json['user']['name']
+            "User":tweet._json['user']['name'],
+            "Text":tweet._json['text'],
+            "Sentiment": analyzer.polarity_scores(tweet._json['text'])  
         })
     return tweet_data
 
